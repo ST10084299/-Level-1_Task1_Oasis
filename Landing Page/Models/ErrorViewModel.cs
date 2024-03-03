c13ac6c25a96fb1cf3b3db068ab8f1937ef23ad9
ST10084299 <izakariya03@gmail.com>
2024-03-03 09:47:07 +0200

    new commit

---
+++ Landing Page/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Landing_Page.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
